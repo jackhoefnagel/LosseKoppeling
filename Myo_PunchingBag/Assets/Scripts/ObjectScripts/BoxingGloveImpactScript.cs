740cf43058914b344c4d67624358f1ebc882c4aa
Jack Hoefnagel <jackhoefnagel@gmail.com>
2016-12-03 01:37:52 +0100

    Graphics Update Punching Bag - Blender 3d model, impact particles, cubemap

---
+++ Myo_PunchingBag/Assets/Scripts/ObjectScripts/BoxingGloveImpactScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxingGloveImpactScript : MonoBehaviour {
+
+    public ParticleSystem punchImpactParticles;
+
+    void Awake(){
+        punchImpactParticles.Stop();
+    }
+
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.CompareTag("Object")){
+            //if velocity magnitude > ?
+            punchImpactParticles.Play();
+        }
+    }
+}
